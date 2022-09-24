3938c0cb771bdd2d9cf812b15fe89e36c69c10bb
SeanICho <ihcho131313@gmail.com>
2022-09-24 21:45:07 +0000

    view, controller, middleware - initial setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Demo\DemoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
+});
+
+// Route::get('/about', function () {
+//     return view('about');
+// });
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+
+Route::controller(DemoController::class)->group(function () {
+	Route::get('/about', 'About')->name('about.page')->middleware('check');
+	Route::get('/contact', 'Contact')->name('contact.page');
 });
